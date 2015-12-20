faac9146463081472e49f304a9cfbf338e34f9bd
Alejandro Steinmetz <asteinmetz78@gmail.com>
2015-12-20 17:55:54 -0300

    First commit

---
+++ AlesteAdminLTEGeneratorBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ *
+ * (c) Alejandro Steinmetz <asteinmetz78@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aleste\GeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AlesteAdminLTEGeneratorBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'SensioGeneratorBundle';
+    }
+}
